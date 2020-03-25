3c12cbbbe14b0ec7873d530334b13a0bcc995b89
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2020-03-25 18:33:25 +0100

    Adding Command Handler

---
+++ BackingUpConsole/BackingUpConsole/Utilities/Command.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackingUpConsole.Utilities.Commands
+{
+    public class Command
+    {
+        //Attributes
+        public readonly string cmd;
+        //Constructors
+        public Command(string cmd)
+        {
+            this.cmd = cmd;
+        }
+
+        //overrides
+        public override bool Equals(object obj)
+        {
+            return this == (Command)obj;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return cmd;
+        }
+
+
+        public static bool operator ==(Command left, Command right) => left.cmd == right.cmd;
+        public static bool operator !=(Command left, Command right) => !(left == right);
+    }
+}
---
+++ BackingUpConsole/BackingUpConsole/Utilities/CommandCollections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackingUpConsole.Utilities.Commands
+{
+    public class CommandCollections
+    {
+        public static Command RunFile => new Command("run");
+
+        public static Command GetCommand(string cmd) => new Command(cmd);
+    }
+}
