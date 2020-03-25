a1e3109f72d967a0095c72091967f284fb77e10e
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2020-03-25 14:23:28 +0100

    Add message handler

---
+++ BackingUpConsole/BackingUpConsole/Utilities/MessageCollections.cs
@@ -0,0 +1,18 @@
+﻿namespace BackingUpConsole.Utilities.Messages
+{
+    public class MessageCollections
+    {
+        public enum Codes
+        {
+            QuitProgram
+        }
+        public enum Levels
+        {
+            Fatal,
+            Error,
+            Warning,
+            Information,
+            Debug
+        }
+    }
+}
---
+++ BackingUpConsole/BackingUpConsole/Utilities/MessageHandler.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+using System;
+
+namespace BackingUpConsole.Utilities.Messages
+{
+    public class MessageHandler
+    {
+        //Attributes
+        public MessageCollections.Codes Code { get; }
+        public string Message { get; }
+        public MessageCollections.Levels Level { get; }
+        public ConsoleColor? Color { get; }
+        //Constructor
+        public MessageHandler(MessageCollections.Codes code, string message, MessageCollections.Levels level, ConsoleColor color)
+        {
+            Code = code;
+            Message = message;
+            Level = level;
+            Color = color;
+        }
+        public MessageHandler(MessageCollections.Codes code, string message, MessageCollections.Levels level)
+        {
+            Code = code;
+            Message = message;
+            Level = level;
+        }
+        //overriding operators
+        public static bool operator ==(MessageHandler left, MessageHandler right) => (left.Code == right.Code);
+        public static bool operator !=(MessageHandler left, MessageHandler right) => !(left == right);
+    }
+}
