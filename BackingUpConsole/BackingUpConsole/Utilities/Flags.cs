140480417b65cc6af1c920bd2c698606084802ec
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2020-03-25 18:32:22 +0100

    Introducing Flags system

---
+++ BackingUpConsole/BackingUpConsole/Utilities/Flags.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BackingUpConsole.Utilities
+{
+    public class Flags
+    {
+        public static UInt16 ONLY_COMPILE =>    0b0000_0000_0000_0001;
+        public static UInt16 CHAIN_COMPILE =>   0b0000_0000_0000_0010;
+    }
+}
