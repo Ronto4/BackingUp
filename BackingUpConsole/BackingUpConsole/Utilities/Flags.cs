6e1c957fc9a144082e4613c513849fdb1aff6b79
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2020-03-25 23:54:07 +0100

    Add another flag

--- BackingUpConsole/BackingUpConsole/Utilities/Flags.cs
+++ BackingUpConsole/BackingUpConsole/Utilities/Flags.cs
@@ -6,5 +6,6 @@ namespace BackingUpConsole.Utilities
     {
         public static UInt16 ONLY_COMPILE => 0b0000_0000_0000_0001;
         public static UInt16 CHAIN_COMPILE => 0b0000_0000_0000_0010;
+        public static UInt16 NO_COMPILE => 0b0000_0000_0000_0100;
     }
 }
