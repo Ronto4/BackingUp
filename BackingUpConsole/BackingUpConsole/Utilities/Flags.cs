ecd42faf595fac1b841614315b22a66d8f4352f2
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2020-03-31 18:43:39 +0200

    Set verbose on default

--- BackingUpConsole/BackingUpConsole/Utilities/Flags.cs
+++ BackingUpConsole/BackingUpConsole/Utilities/Flags.cs
@@ -12,7 +12,7 @@ namespace BackingUpConsole.Utilities
         public static UInt16 CHAIN_COMPILE => 0b0000_0000_0000_0100;
         public static UInt16 VERBOSE => 0b0000_0000_0000_1000;
 
-        public static UInt16 DEFAULT_FLAGS => (UInt16)(0x0 | RUN | COMPILE | CHAIN_COMPILE);
+        public static UInt16 DEFAULT_FLAGS => (UInt16)(0x0 | RUN | COMPILE | CHAIN_COMPILE | VERBOSE);
 
         private static readonly Dictionary<string, UInt16> flagIdentifier = new Dictionary<string, ushort>()
         {
