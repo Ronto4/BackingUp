1d0e3441aaa0d79a8419a86c6d17edc9e75a1f1f
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2020-04-08 13:27:03 +0200

    Dummy class 'BackUpSettings'

---
+++ BackingUpConsole/BackingUpConsole/CoreFunctions/BackUpSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackingUpConsole.CoreFunctions
+{
+    class BackUpSettings
+    {
+        //Attributes
+        public string Path { get; }
+        //Constructors
+        public BackUpSettings(string path)
+        {
+            Path = path;
+        }
+    }
+}
