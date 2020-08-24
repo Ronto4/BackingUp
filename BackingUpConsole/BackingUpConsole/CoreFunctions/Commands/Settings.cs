082d69eb3270d75cfad8715166afc077353e4f0a
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2020-08-24 19:44:41 +0200

    Add checks to conform to command structure

--- BackingUpConsole/BackingUpConsole/CoreFunctions/Commands/Settings.cs
+++ BackingUpConsole/BackingUpConsole/CoreFunctions/Commands/Settings.cs
@@ -11,7 +11,7 @@ namespace BackingUpConsole.CoreFunctions.Commands
     {
         public static MessageHandler Parse(string[] args, UInt16 flags, Paths paths, MessagePrinter messagePrinter)
         {
-            if (!args.CheckLength(1, 1) && !args.CheckLength(4, 4))
+            if (!args.CheckLength(1, 4))
                 return MessageProvider.IncorrectArgumentCount(!flags.IsSet(Flags.VERBOSE));
 
             if (paths.SelectedBackup is null)
@@ -21,7 +21,38 @@ namespace BackingUpConsole.CoreFunctions.Commands
                 return MessageProvider.Success();
 
             if (!paths.SelectedBackup.Settings.ParameterExists(args[1]))
+            {
+                if (args[1] == "path")
+                {
+                    if (args.CheckLength(2, 2))
+                        return MessageProvider.Success();
+                    return MessageProvider.IncorrectArgumentCount(!flags.IsSet(Flags.VERBOSE));
+                }
+                if (args[1] == "select")
+                {
+                    if (args.CheckLength(3, 3))
+                        return MessageProvider.Success();
+                    return MessageProvider.IncorrectArgumentCount(!flags.IsSet(Flags.VERBOSE));
+                }
+                if (args[1] == "create")
+                {
+                    if (args.CheckLength(3, 4))     // TODO: Check if args[3] (from-name) is an existing settings file
+                        return MessageProvider.Success();
+                    return MessageProvider.IncorrectArgumentCount(!flags.IsSet(Flags.VERBOSE));
+                }
+                if (args[1] == "remove")
+                {
+                    if (args.CheckLength(2, 3))     // TODO: Check if args[2] (name) is an existing settings file and does not match the currently selected settings
+                        return MessageProvider.Success();
+                    return MessageProvider.IncorrectArgumentCount(!flags.IsSet(Flags.VERBOSE));
+                }
                 return MessageProvider.ParameterDoesNotExist(args[1]);
+            }
+            if (args.CheckLength(2, 2))
+                return MessageProvider.Success();
+
+            if (args.CheckLength(3, 3))
+                return MessageProvider.IncorrectArgumentCount(!flags.IsSet(Flags.VERBOSE));
 
             if (BackUpSettings.EditTypes.ContainsKey(args[2]) == false)
                 return MessageProvider.UnknownSettingsUsage(args[2], !flags.IsSet(Flags.VERBOSE));
