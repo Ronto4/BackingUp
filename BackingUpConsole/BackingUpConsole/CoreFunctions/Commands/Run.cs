fdd32e02aa5bb55eb072224ec1c949abcb17023e
Clemens Schielicke <clemens.schielicke@gmail.com>
2020-11-01 15:52:02 +0100

    Add basic file structure

---
+++ BackingUpConsole/BackingUpConsole/CoreFunctions/Commands/Run.cs
@@ -0,0 +1,21 @@
+﻿using BackingUpConsole.Utilities;
+using BackingUpConsole.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackingUpConsole.CoreFunctions.Commands
+{
+    internal static class Run
+    {
+        public static MessageHandler Parse(string[] args, UInt16 flags, Paths paths, MessagePrinter messagePrinter)
+        {
+            return MessageProvider.ParseSuccess();
+        }
+        public static async Task<MessageHandler> RunAsync(string[] args, UInt16 flags, Paths paths, MessagePrinter messagePrinter)
+        {
+            return MessageProvider.Success();
+        }
+    }
+}
