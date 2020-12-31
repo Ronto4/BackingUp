ba37340be0aa4ec9e9d0e8e119ea908f3aa53191
Clemens Schielicke <clemens.schielicke@gmail.com>
2020-12-31 16:30:01 +0100

    Support multiple paths in settings file

--- BackingUpConsole/BackingUpConsole/CoreFunctions/BackingUp/Handler.cs
+++ BackingUpConsole/BackingUpConsole/CoreFunctions/BackingUp/Handler.cs
@@ -7,7 +7,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -54,13 +53,13 @@ namespace BackingUpConsole.CoreFunctions.BackingUp
             if (useSequential)
             {
                 sw.Start();
-                await SequentialBackup.PerformBackup(new DirectoryInfo(backUp.Settings!.Settings.Paths[0]), new DirectoryInfo(backUp.FileContainer.DataDir), messagePrinter);
+                await SequentialBackup.PerformBackup(backUp.Settings!.Settings.Paths.Select(path => new DirectoryInfo(path)), new DirectoryInfo(backUp.FileContainer.DataDir), messagePrinter);
                 sw.Stop();
             }
             else
             {
                 sw.Start();
-                await Parallel.PerformBackup(new DirectoryInfo(backUp.Settings!.Settings.Paths[0]), new DirectoryInfo(backUp.FileContainer.DataDir), messagePrinter);
+                await Parallel.PerformBackup(backUp.Settings!.Settings.Paths.Select(path => new DirectoryInfo(path)), new DirectoryInfo(backUp.FileContainer.DataDir), messagePrinter);
                 sw.Stop();
             }
             return MessageProvider.Message($"The process took {sw.ElapsedMilliseconds} ms in {(useSequential ? "sequential" : "parallel")} mode with a MaxBlockSize of {MaxBlockSize}.");
@@ -104,19 +103,14 @@ namespace BackingUpConsole.CoreFunctions.BackingUp
                 bool differenceFound = (await Miscellaneous.FilesAreIdentical(file, target, MaxBlockSize)) == false;
                 if (differenceFound)
                 {
-                    byte[] msBuffer = new byte[MaxBlockSize];
+                    byte[] buffer = new byte[MaxBlockSize];
                     using FileStream sourceStream = new FileStream(file.FullName, FileMode.Open);
-                    //using StreamWriter targetStream = new StreamWriter(Miscellaneous.CreateFileAndDirectoryIfNotExist(target));
                     using FileStream targetStream = Miscellaneous.CreateFileAndDirectoryIfNotExist(target);
-                    //using MemoryStream sourceMSStream = new MemoryStream(msBuffer);
-                    //char[] sourceBuffer = new char[MaxBlockSize];
                     int read = 1;
                     while (read > 0)
                     {
-                        read = await sourceStream.ReadAsync(msBuffer, 0, MaxBlockSize);
-                        Task write = targetStream.WriteAsync(msBuffer, 0, read);
-                        //int saved = await sourceStream.ReadAsync(sourceBuffer, 0, MaxBlockSize);
-                        //Task write = targetStream.WriteAsync(sourceBuffer, 0, saved);
+                        read = await sourceStream.ReadAsync(buffer, 0, MaxBlockSize);
+                        Task write = targetStream.WriteAsync(buffer, 0, read);
                         if (doArchive)
                         {
                             // Process archive, still missing.
@@ -132,34 +126,38 @@ namespace BackingUpConsole.CoreFunctions.BackingUp
             }
             catch (Exception ex)
             {
-                return MessageProvider.Message($"An error occurred while backing up file at '{file.FullName}':{Environment.NewLine}{ex.Message}", MessageCollections.Levels.Error);
+                return MessageProvider.Message($"An error occurred while backing up file at '{file.FullName}':{Environment.NewLine}\t{ex.Message}", MessageCollections.Levels.Error);
             }
         }
 
         private static class Parallel
         {
-            private static async Task<List<FileInfo>> EnumerateAllFilesRecursively(DirectoryInfo root, CancellationToken cancellationToken)
+            private static async Task<List<FileInfo>> EnumerateAllFilesRecursively(DirectoryInfo root, CancellationToken cancellationToken) => await EnumerateAllFilesRecursively(new DirectoryInfo[] { root }, cancellationToken);
+            private static async Task<List<FileInfo>> EnumerateAllFilesRecursively(IEnumerable<DirectoryInfo> roots, CancellationToken cancellationToken)
             {
                 List<FileInfo> files = new List<FileInfo>();
-                try
+                foreach (DirectoryInfo root in roots)
                 {
-                    foreach (DirectoryInfo directory in root.EnumerateDirectories())
+                    try
                     {
-                        files.AddRange(await EnumerateAllFilesRecursively(directory, cancellationToken));
+                        foreach (DirectoryInfo directory in root.EnumerateDirectories())
+                        {
+                            files.AddRange(await EnumerateAllFilesRecursively(directory, cancellationToken));
+                        }
                     }
+                    catch { }
+                    try
+                    {
+                        files.AddRange(root.EnumerateFiles());
+                    }
+                    catch { }
                 }
-                catch { }
-                try
-                {
-                    files.AddRange(root.EnumerateFiles());
-                }
-                catch { }
                 return files;
             }
-            private static async IAsyncEnumerable<MessageHandler> BackupDirectory(DirectoryInfo directory, DirectoryInfo backupDir, [EnumeratorCancellation] CancellationToken cancellationToken)
+            private static async IAsyncEnumerable<MessageHandler> BackupDirectory(IEnumerable<DirectoryInfo> directories, DirectoryInfo backupDir, [EnumeratorCancellation] CancellationToken cancellationToken)
             {
                 yield return MessageProvider.Message("Beginning to enumerate all files...");
-                List<FileInfo> files = await EnumerateAllFilesRecursively(directory, cancellationToken);
+                List<FileInfo> files = await EnumerateAllFilesRecursively(directories, cancellationToken);
                 yield return MessageProvider.Message($"Enumerating done! Found {files.Count} files.");
                 List<Task<MessageHandler>> tasks = new List<Task<MessageHandler>>();
                 tasks.AddRange(files.Select(file => Task.Run(() => BackUpFile(file, new FileInfo(PathHandler.Combine(backupDir.FullName, file.FullName.Replace(':', '-'))), false, cancellationToken))));
@@ -176,9 +174,9 @@ namespace BackingUpConsole.CoreFunctions.BackingUp
                 //    yield return await message;
                 //}
             }
-            internal static async Task PerformBackup(DirectoryInfo backupRoot, DirectoryInfo backupDir, MessagePrinter messagePrinter)
+            internal static async Task PerformBackup(IEnumerable<DirectoryInfo> backupRoots, DirectoryInfo backupDir, MessagePrinter messagePrinter)
             {
-                await foreach (MessageHandler message in BackupDirectory(backupRoot, backupDir, new CancellationTokenSource().Token))
+                await foreach (MessageHandler message in BackupDirectory(backupRoots, backupDir, new CancellationTokenSource().Token))
                 {
                     messagePrinter.Print(message);
                 }
@@ -261,9 +259,10 @@ namespace BackingUpConsole.CoreFunctions.BackingUp
                 }
                 catch { }
             }
-            internal static async Task PerformBackup(DirectoryInfo backupRoot, DirectoryInfo backupDir, MessagePrinter messagePrinter)
+            internal static async Task PerformBackup(IEnumerable<DirectoryInfo> backupRoots, DirectoryInfo backupDir, MessagePrinter messagePrinter)
             {
-                await Dir(backupRoot, backupDir, false, messagePrinter, new CancellationTokenSource().Token);
+                foreach (DirectoryInfo backupRoot in backupRoots)
+                    await Dir(backupRoot, backupDir, false, messagePrinter, new CancellationTokenSource().Token);
             }
             //private static bool? File(FileInfo file)
             //{
